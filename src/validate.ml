8a7dea199daa0b08275e1b9c50c27e240a85bebf
Batuhan TANIR <128717319+batuhantanir@users.noreply.github.com>
2026-07-28 17:33:03 +0300

    fix validate problem check duplication

--- src/validate.ml
+++ src/validate.ml
@@ -2,23 +2,37 @@ open Errors
 open Types
 
 let validate_machine m =
+  let rec check_duplicates = function
+    | [] -> ()
+    | (c:string) :: rest -> 
+        if List.mem c rest then
+          raise (Invalid_machine (Printf.sprintf "alphabet contains duplicate character: '%s'" c))
+        else
+          check_duplicates rest
+  in
+
   let fail fmt = Printf.ksprintf (fun s -> raise (Invalid_machine s)) fmt in
   if m.alphabet = [] then fail "alphabet must not be empty";
-  List.iter
-    (fun s ->
-      if String.length s <> 1 then
-        fail "alphabet character '%s' must have length exactly 1" s)
-    m.alphabet;
+
+  if not (List.for_all (fun s -> String.length s = 1) m.alphabet) then
+    fail "all alphabet characters must have length exactly 1";
+  
   if not (List.mem m.blank m.alphabet) then
     fail "blank character '%s' must be part of the alphabet" m.blank;
+  
+  check_duplicates m.alphabet;
+
   if m.states = [] then fail "states must not be empty";
+
   if not (List.mem m.initial m.states) then
     fail "initial state '%s' must be part of the states list" m.initial;
+
   List.iter
     (fun f ->
       if not (List.mem f m.states) then
         fail "final state '%s' must be part of the states list" f)
     m.finals;
+    
   List.iter
     (fun (state, trs) ->
       if not (List.mem state m.states) then
